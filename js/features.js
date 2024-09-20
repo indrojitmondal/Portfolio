3d4517e88a0c9e67580c64e1ba8b5a56c477a529
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-20 12:10:38 +0600

    updated in projects

--- js/features.js
+++ js/features.js
@@ -20,3 +20,35 @@ function showFeaturesInLargeScreenById(id){
 
     document.getElementById(id).classList.add('text-primary');
 }
+
+// document.getElementById('mobile').classList.add('bg-primary');
+// document.getElementById('mobile').classList.add('text-white');
+
+let projectsArray= document.querySelectorAll('#projects-container button');
+
+    
+
+ 
+for (const project of projectsArray) {
+    
+    project.classList.add('text-primary');
+    project.classList.remove('bg-primary');
+
+}
+
+function styleProjectSectionByID(id){
+
+    for (const project of projectsArray) {
+    
+  
+        project.classList.remove('bg-primary');
+        project.classList.remove('text-white');
+    
+    }
+
+
+    document.getElementById(id).classList.add('bg-primary');
+    document.getElementById(id).classList.add('text-white');
+
+
+}
--- js/projects.js
+++ js/projects.js
@@ -2,3 +2,32 @@ console.log('projects.js Connected');
  
 showFeaturesInLargeScreenById(idArray[1]);
 console.log(idArray[1]);
+
+document.getElementById('web-application').addEventListener('click', function (){
+    
+    styleProjectSectionByID('web-application');
+    
+   
+    
+
+})
+document.getElementById('mobile-application').addEventListener('click', function (){
+  
+    styleProjectSectionByID('mobile-application');  
+})
+document.getElementById('game-development').addEventListener('click', function (){
+  
+    styleProjectSectionByID('game-development');  
+})
+document.getElementById('data-structures').addEventListener('click', function (){
+  
+    styleProjectSectionByID('data-structures');  
+})
+document.getElementById('algorithms').addEventListener('click', function (){
+  
+    styleProjectSectionByID('algorithms');  
+})
+document.getElementById('all').addEventListener('click', function (){
+  
+    styleProjectSectionByID('all');  
+})
