36301ac2ac6837a182e0268d2137250814a78e5f
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-11 09:25:00 +0600

    added projects in index.html

--- js/index.js
+++ js/index.js
@@ -2,3 +2,11 @@ console.log('index.js Connected');
  
 // showFeaturesInLargeScreenById(idArray[0]);
 // console.log(idArray[0]);
+
+const projectButtons = document.querySelectorAll('#webApp button');
+console.log(projectButtons.length);
+for (const btn of projectButtons) {
+
+    btn.classList.add('hover:bg-hello','hover:text-white');
+    
+}
